a18948af10d4ef9b9b37420f65ef85a142d2802d
Karl Fredin <karlfredin@gmail.com>
2022-12-02 16:41:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# Setup - Utility that will configure my desktop
+
+Utility that will configure your desktop using my configuration files
+
+My plan is to make this work on any distro but currently its just in testing phase and isn't that stable, But this script will work best on arch for now
+
+# OBS This project is work in progress use on your own risk (I recommendnd to not use it at all not working right now!!!)
+## Currently testing on 
+
+Arch 
+
+Ubuntu
+
+Debian
+
+Fedora
+
+## What this script does
+
+This script includes the following
+All config files will be pulled from my dotfiles repository
+
+Qtile - Window Manager
+
+Kitty - Terminal emulator
+
+Neovim - Text Editor
+
+Goneovim - Neovim Graphical Program
+
+Neomutt (If you use arch it will also install muttwizard) - Terminal Mail Client
+
+SDDM - Displaymanager
+
+Rofi - Run Launcher
+
+![rofi](https://i.imgur.com/qy1yTFX.png)
+
+Slock - Lockscreen
+
+![slock](https://i.imgur.com/74fazMq.png)
+
+Tmux with OH-MY-TMUX - Terminal Multiplexer 
+
+Vifm - Terminal FileManager
+
+PcManFM - Graphical FileManager
+
+OH-MY-ZSH - Plugin Manager for ZSH
+
+Fish,Zsh and Bash - you get to choose the Default
+
+Starship - Crossplatform Shell Prompt
+
+Choose between firefox,qutebrowser,chromium or brave as your browser
+
+Grub Theme (Only have it tested and working on Debian and Arch)
+
+BTOP - Terminal System Resource Monitor
+
+Docker - Container Virtulization 
+
+OBS this script also deactivates root password prompt when using sudo this will be a security risk I just prefer it because I'm lazy
+
+Some of these Programs are optional and the script will prompt you if you want to install it or not. 
+
+## This script installs some GTK themes which includes
+
+Dracula
+
+## Information for Arch Users
+
+If you need to install a fresh version of arch linux this script can do it for you automatically.
+All you need to do is to answer a few prompt for example which disk you want to use for the installation.
+The script will detect that you use the archiso so you can just run the same script
+
+1. wget https://raw.githubusercontent.com/phoenix988/setup/main/config.sh
+2. bash config.sh
+3. Reboot
+4. Now you can customize your new installation by hand or clone this repo and run this script again.
+
+## Important Info!!!
+
+This repo is mainly for me to make it easier for me when I reinstall
+
+The run.sh script is broken it doesnt work, I also wanted to be able to run this script on a remote machine but at the moment that doesnt work
+
+This is in Beta I recommend to not use this on a production machine use it in  VM or Testing Machine
+
+## In order to use this script follow these steps
+
+1. git clone https://github.com/phoenix988/setup.git 
+2. cd setup
+3. ./config.sh
+
+
+And lastly just follow the prompts this script will ask you some questions before it run
+
+
---
+++ arch-chroot.sh
@@ -0,0 +1,131 @@
+#!/bin/bash              
+
+error() { \
+    clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;
+}
+
+user=$(awk -F = '{if(NR==1) print $NF}' /root/.bashrc )
+host_name=$(awk -F = '{if(NR==2) print $NF}' /root/.bashrc )
+bios_version=$(awk -F = '{if(NR==3) print $NF}' /root/.bashrc )
+efidrive=$(awk -F = '{if(NR==4) print $NF}' /root/.bashrc )
+biosdrive=$(awk -F = '{if(NR==5) print $NF}' /root/.bashrc )
+
+ 
+echo "######################"
+echo "## Updating Locales ##"
+echo "######################"
+sed -i 's/#en_GB.UTF-8/en_GB.UTF-8/g' /etc/locale.gen
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+locale-gen &> /dev/null
+
+sleep 2
+clear
+
+echo "#########################"
+echo "## Setting up timezone ##"
+echo "#########################"
+ln -sf /usr/share/zoneinfo/Europe/Vilnius /etc/localtime
+
+sleep 2
+clear
+
+echo "#########################"
+echo "## Enabling networking ##"
+echo "#########################"
+systemctl enable NetworkManager &> /dev/null
+
+sleep 2
+clear
+
+echo "#################"
+echo "## Adding User ##"
+echo "#################"
+useradd $user
+passwd $user
+usermod -aG wheel $user   
+[ -d /home/$user ] || mkdir /home/$user
+[ -d /home/$user ] && chown karl:karl -R /home/$user
+
+sleep 2
+clear
+
+echo "############################"
+echo "## Modifying Sudoers file ##"
+echo "############################"
+echo '%wheel ALL=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo &> /dev/null 
+
+sleep 2   
+clear
+
+echo "#########################"
+echo "## Setting up Hostname ##"
+echo "#########################"
+echo "$host_name" > /etc/hostname 
+
+sleep 2
+clear
+
+
+if [ "$bios_version" = "U" -o "$bios_version" = "u" ] ; then
+
+        [ -d /boot/EFI ] || mkdir /boot/EFI
+        
+    
+        mount $efidrive /boot/EFI &> /dev/null
+      
+        echo "#####################"
+        echo "## Installing Grub ##"
+        echo "#####################"
+
+
+        grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB || error "Failed to install grub"
+       
+    
+        genfstab -U / > /etc/fstab
+        
+        sleep 2
+        clear
+    
+        if [ $? = "0" ] ; then
+
+             echo "" &> /dev/null
+       
+        else 
+
+             exit
+
+         fi
+
+else
+
+        echo "#####################"
+        echo "## Installing Grub ##"
+        echo "#####################"
+         
+        grub-install --target=i386-pc $biosdrive || error "Failed to install grub"
+
+        
+        sleep 2
+        clear 
+
+        if [ $? = "0" ] ; then
+
+            echo "" &> /dev/null
+            
+        else 
+
+             exit
+
+        fi
+
+
+fi   
+ 
+echo "#########################"
+echo "## Generating grub.cfg ##"
+echo "#########################"
+
+grub-mkconfig -o /boot/grub/grub.cfg 
+
+sleep 2
+clear
