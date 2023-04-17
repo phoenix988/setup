f75c8eca2a831d1c8f6fd6ffa5d298c8b765f94b
phoenix988 <karlfredin@gmail.com>
2023-04-17 19:25:36 +0300

    fixing error

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -123,9 +123,15 @@ bash ./zsh-plugins.sh
 clear
 
 # Install starship prompt
-sh ./starship.sh
+
+command_exist starship || sh ./starship.sh
 
 clear
+
+# Installs packer for neovim cause I can't get bootstarp to work
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim &> /dev/null
+
 
 for FILES in $(printf '%s\n' "${!files[@]}") ; do
 
